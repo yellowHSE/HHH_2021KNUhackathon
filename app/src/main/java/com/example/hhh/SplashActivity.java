103e40a4dbaf129e1e423b4987307d8d719b04f9
hdddhdd <nuly08@naver.com>
2021-07-20 23:38:47 +0900

    splash

---
+++ app/src/main/java/com/example/hhh/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.example.hhh;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        try{
+            Thread.sleep(4000);
+        }catch(InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
+}
