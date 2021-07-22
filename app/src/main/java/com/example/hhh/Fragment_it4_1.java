90a2bbdb8f20199a46d596b2a5ed409cf6b2d999
yellowHSE <74814515+yellowHSE@users.noreply.github.com>
2021-07-23 01:25:21 +0900

    Add files via upload

---
+++ app/src/main/java/com/example/hhh/Fragment_it4_1.java
@@ -0,0 +1,19 @@
+package com.example.staff_reservation;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.support.annotation.Nullable;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+
+public class Fragment_it4_1 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_it4_1_layout,container, false);
+    }
+
+}
